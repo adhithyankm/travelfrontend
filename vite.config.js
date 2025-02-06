0e5d8d88988b2d1544a2788369b33823067ed619
adhithyankm <139346873+adhithyankm@users.noreply.github.com>
2025-02-06 12:34:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base:'travelfrontend/',
+  base:'https://adhithyankm.github.io/travelfrontend/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
