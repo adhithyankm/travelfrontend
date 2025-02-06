7ba91dcda443f8fac5be5d1417438a4db88989f6
adhithyankm <139346873+adhithyankm@users.noreply.github.com>
2025-02-06 14:25:39 +0530

    Update HomePage.vue

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -78,7 +78,7 @@ const tours = [ // deployed one path
   {
     title: 'Karnataka Tour',
     subtitle: 'Mysore Palace, Karnataka',
-    image: "./assets/pexels-k3ithvision-4134644.jpg", 
+    image: "../../dist/assets/img/pexels-k3ithvision-4134644.jpg", 
   },
   {
     title: 'Kargil Tour',
